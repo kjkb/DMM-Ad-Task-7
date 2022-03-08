36d433f963264ad188ef396a7a62aecbc66c8df6
kjkubo <98828564+kjkubo@users.noreply.github.com>
2022-03-08 08:52:56 +0000

    [Finish]Ad-8b

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,10 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def create
